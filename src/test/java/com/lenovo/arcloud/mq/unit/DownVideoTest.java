867884935b119c2f48b9fac79b1c2d6687232283
3w <www6v@126.com>
2018-09-17 17:10:44 +0800

    test

--- src/test/java/com/lenovo/arcloud/mq/unit/DownVideoTest.java
+++ src/test/java/com/lenovo/arcloud/mq/unit/DownVideoTest.java
@@ -42,12 +42,12 @@ public class DownVideoTest {
             /// trainingData.zip  建冲
 
             /// 阿里云 42
-            for(int i=0; i<1; i++) {
-                String body = Unirest.post("http://120.55.162.42:8103/topic/downVideo.ar").header("accept", "application/json")
-                        .field("messageBody", "{'video.url':'http://repo.shai.cloud:18080/api/app/fs/contents/20180420181143.zip','record.id':'11111','alg.name':'ImageFeature_ORB','flow.name':'Reconstruction', 'developerId':'149'}").asString().getBody();
-                System.out.println(body);
-                Thread.sleep(1000*10);
-            }
+//            for(int i=0; i<1; i++) { ///   repo.shai.cloud
+//                String body = Unirest.post("http://120.55.162.42:8103/topic/downVideo.ar").header("accept", "application/json")
+//                        .field("messageBody", "{'video.url':'http://repo.shai.cloud:18080/api/app/fs/contents/20180420181143.zip','record.id':'11111','alg.name':'ImageFeature_ORB','flow.name':'Reconstruction', 'developerId':'149'}").asString().getBody();
+//                System.out.println(body);
+//                Thread.sleep(1000*10);
+//            }
 
             /// test env
 //            for(int i=0; i<1; i++) {
@@ -63,12 +63,13 @@ public class DownVideoTest {
 //                System.out.println(body);
 //            }
 
-            /// deeplearning 151
-//            for(int i=0; i<1; i++) {
-//                String body = Unirest.post("http://10.4.65.151:8103/topic/downVideo.ar").header("accept", "application/json")
-//                        .field("messageBody", "{'video.url':'http://repo.shai.cloud:18080/api/app/fs/contents/20180420181143.zip','record.id':'11111','alg.name':'ImageFeature_ORB','flow.name':'Reconstruction', 'developerId':'149'}").asString().getBody();
-//                System.out.println(body);
-//            }
+            /// deeplearning 151 -> 10.4.65.151   gpu.shai.cloud
+            for(int i=0; i<1; i++) {
+                /// 11111
+                String body = Unirest.post("http://gpu.shai.cloud:8103/topic/downVideo.ar").header("accept", "application/json")
+                        .field("messageBody", "{'video.url':'http://repo.shai.cloud:18080/api/app/fs/contents/20180420181143.zip','record.id':'1174','alg.name':'ImageFeature_ORB','flow.name':'Reconstruction', 'developerId':'149'}").asString().getBody();
+                System.out.println(body);
+            }
 
             /// 'video.url':'http://120.55.162.42:8080/hdpSoftware/20180420181143.zip'
             ///http://repo.shai.cloud:8180/api/app/fs/contents/20180516164032-cupww.zip
